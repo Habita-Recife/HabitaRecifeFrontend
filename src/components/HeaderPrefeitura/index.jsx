fff912679bb96183dfc9351f8a23506647007ccc
ViniciusOliveiraDev <viniciuscontato2024@gmail.com>
2025-04-10 16:15:18 -0300

    update

--- src/components/HeaderPrefeitura/index.jsx
+++ src/components/HeaderPrefeitura/index.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { LogOut, Lock, ChevronDown} from "lucide-react";
+import { LogOut, Lock, ChevronDown } from "lucide-react";
 import logo from "../../assets/logo05.png";
 import { converterRoles } from '../../utils/utils';
 import { useAuth } from "../../contexts/AuthContext";
@@ -17,21 +17,21 @@ const HeaderPrefeitura = () => {
         setIsDropdownOpen(false);
       }
     };
-  
+
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
-  
+
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
-  
+
   const handleLogout = () => {
     if (accessToken) {
       logOut();
-      navigate("/login");  
+      navigate("/login");
     }
   };
 
@@ -42,12 +42,12 @@ const HeaderPrefeitura = () => {
   return (
     <div className="w-full h-20 bg-[#2C3E50] flex items-center justify-between px-8 shadow-lg">
       <div className="flex items-center gap-10 justify-center">
-          <div className="flex flex-col items-center gap-3">
-            <img src={logo} alt="Logo" className="w-35   h-14" />
-          </div>
+        <div className="flex flex-col items-center gap-3">
+          <img src={logo} alt="Logo" className="w-35   h-14" />
+        </div>
       </div>
-      
-      <div className="flex items-center gap-4 cursor-pointer group" 
+
+      <div className="flex items-center gap-4 cursor-pointer group"
         onClick={toggleDropdown}
         ref={dropdownRef}>
         <div className="w-12 h-12 rounded-full bg-[#008080] flex items-center justify-center text-white font-bold text-xl group-hover:bg-white group-hover:text-[#008080] transition-colors duration-300 shadow-md">
@@ -58,14 +58,13 @@ const HeaderPrefeitura = () => {
             {user.username?.toUpperCase() || ''}
           </span>
           <span className="text-gray-300 text-sm group-hover:text-white transition-colors duration-300">
-            {converterRoles(user?.roles?.[0] || '')}
+            ADMIN
           </span>
         </div>
-        <ChevronDown 
-          size={18} 
-          className={`text-white opacity-70 group-hover:text-[#008080] transition-all duration-300 ${
-            isDropdownOpen ? "transform rotate-180" : ""
-          }`} 
+        <ChevronDown
+          size={18}
+          className={`text-white opacity-70 group-hover:text-[#008080] transition-all duration-300 ${isDropdownOpen ? "transform rotate-180" : ""
+            }`}
         />
 
         {isDropdownOpen && (
@@ -83,7 +82,7 @@ const HeaderPrefeitura = () => {
             </div>
 
             <div className="py-1">
-              <button 
+              <button
                 onClick={handleChangePassword}
                 className="flex items-center w-full px-4 py-2 text-gray-700 hover:bg-gray-100 text-left"
               >
@@ -93,7 +92,7 @@ const HeaderPrefeitura = () => {
             </div>
 
             <div className="border-t border-gray-200">
-              <button 
+              <button
                 onClick={handleLogout}
                 className="flex items-center w-full px-4 py-2 text-red-600 hover:bg-red-50"
               >
